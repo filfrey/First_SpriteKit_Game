8d796b23a29df4af38c9aad6fb50ab91e9e2be6a
filfrey <JeffreyLin1219@gmail.com>
2015-06-25 17:50:19 -0400

    Added Main Menu

--- First_SpriteKit_Game/EnemySprite.swift
+++ First_SpriteKit_Game/EnemySprite.swift
@@ -29,4 +29,8 @@ class EnemySprite{
         var range = UInt32(50)..<UInt32(200)
         self.randomFrame = Int(range.startIndex + arc4random_uniform(range.endIndex - range.startIndex + 1))
     }
+    func setLine(){
+        var range = UInt32(50)..<UInt32(200)
+        self.randomFrame = Int(range.startIndex + arc4random_uniform(range.endIndex - range.startIndex + 1))
+    }
 }
---
+++ First_SpriteKit_Game/MainMenu.swift
@@ -0,0 +1,14 @@
+//
+//  MainMenu.swift
+//  Not Like You
+//
+//  Created by Jeffrey Lin on 6/25/15.
+//  Copyright (c) 2015 Jeffrey Lin. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+    
+    
+}
